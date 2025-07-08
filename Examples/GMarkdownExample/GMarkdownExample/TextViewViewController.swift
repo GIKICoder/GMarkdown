0ae49eedc47e61426d9fe960bc839a8dd61ce219
GIKI <810373457@qq.com>
2025-07-08 16:18:36 +0800

    feat: update example code

--- Examples/GMarkdownExample/GMarkdownExample/TextViewViewController.swift
+++ Examples/GMarkdownExample/GMarkdownExample/TextViewViewController.swift
@@ -10,37 +10,170 @@ import GMarkdown
 
 class TextViewViewController: UIViewController {
     
-    let markdownView = MarkdownTextView()
+    private let markdownView = MarkdownTextView()
+    private let menuButton = UIButton(type: .system)
+    private let streamButton = UIButton(type: .system)
+    private var currentMarkdownFile = "markdown"
+    private var displayLink: CADisplayLink?
+    private var currentIndex = 0
+    private var currentContent = ""
+    
+    private let markdownFiles = ["markdown", "markdownv2", "markdownv3", "markdownv4", "markdownv5","markdownLatex"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .lightGray
+        setupUI()
+        setupMarkdown()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
         markdownView.isEditable = false
         markdownView.isScrollEnabled = true
         markdownView.isSelectable = true
         view.addSubview(markdownView)
         
-        markdownView.frame = view.bounds
+        menuButton.setImage(UIImage(systemName: "text.book.closed"), for: .normal)
+        menuButton.backgroundColor = .systemBackground
+        menuButton.layer.cornerRadius = 20
+        menuButton.layer.shadowColor = UIColor.black.cgColor
+        menuButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        menuButton.layer.shadowRadius = 4
+        menuButton.layer.shadowOpacity = 0.1
+        menuButton.addTarget(self, action: #selector(showMenu), for: .touchUpInside)
+        view.addSubview(menuButton)
         
-        setupMarkdown()
+        streamButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        streamButton.backgroundColor = .systemBackground
+        streamButton.layer.cornerRadius = 20
+        streamButton.layer.shadowColor = UIColor.black.cgColor
+        streamButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        streamButton.layer.shadowRadius = 4
+        streamButton.layer.shadowOpacity = 0.1
+        streamButton.addTarget(self, action: #selector(startStreamRendering), for: .touchUpInside)
+        view.addSubview(streamButton)
+        
+        markdownView.translatesAutoresizingMaskIntoConstraints = false
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        streamButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            markdownView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            markdownView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            markdownView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            markdownView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            menuButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            menuButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            menuButton.widthAnchor.constraint(equalToConstant: 40),
+            menuButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            streamButton.topAnchor.constraint(equalTo: menuButton.bottomAnchor, constant: 16),
+            streamButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            streamButton.widthAnchor.constraint(equalToConstant: 40),
+            streamButton.heightAnchor.constraint(equalToConstant: 40)
+        ])
     }
     
     @MainActor
-    func setupMarkdown() {
-        guard let filepath = Bundle.main.path(forResource: "markdown", ofType: nil),
+    private func setupMarkdown() {
+        Task {
+            await loadMarkdown(fileName: currentMarkdownFile)
+        }
+    }
+    
+    @objc private func showMenu() {
+        let alert = UIAlertController(title: "选择Markdown文件", message: nil, preferredStyle: .actionSheet)
+        
+        for file in markdownFiles {
+            let action = UIAlertAction(title: file, style: .default) { [weak self] _ in
+                self?.currentMarkdownFile = file
+                Task { [weak self] in
+                    await self?.loadMarkdown(fileName: file)
+                }
+            }
+            alert.addAction(action)
+        }
+        
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel))
+        
+        if let popover = alert.popoverPresentationController {
+            popover.sourceView = menuButton
+            popover.sourceRect = menuButton.bounds
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    @objc private func startStreamRendering() {
+        stopDisplayLink()
+        currentIndex = 0
+        
+        Task { [weak self] in
+            guard let self = self,
+                  let filepath = Bundle.main.path(forResource: currentMarkdownFile, ofType: nil),
+                  let filecontents = try? String(contentsOfFile: filepath, encoding: .utf8) else {
+                return
+            }
+            self.currentContent = filecontents
+            self.setupDisplayLink()
+        }
+    }
+    
+    private func setupDisplayLink() {
+        displayLink = CADisplayLink(target: self, selector: #selector(updateStreamContent))
+        if #available(iOS 15.0, *) {
+            displayLink?.preferredFrameRateRange = CAFrameRateRange(minimum: 60, maximum: 120)
+        } else {
+            displayLink?.frameInterval = 1
+        }
+        displayLink?.add(to: .main, forMode: .common)
+    }
+    
+    private func stopDisplayLink() {
+        displayLink?.invalidate()
+        displayLink = nil
+    }
+    
+    @objc private func updateStreamContent() {
+        guard currentIndex < currentContent.count else {
+            stopDisplayLink()
+            return
+        }
+        
+        let randomCharCount = Int.random(in: 1...5)
+        let endIndex = min(currentIndex + randomCharCount, currentContent.count)
+        let partialContent = String(currentContent.prefix(endIndex))
+        currentIndex = endIndex
+        
+        Task { [weak self] in
+            guard let self = self else { return }
+            await self.renderMarkdown(partialContent)
+        }
+    }
+    
+    private func loadMarkdown(fileName: String) async {
+        guard let filepath = Bundle.main.path(forResource: fileName, ofType: nil),
               let filecontents = try? String(contentsOfFile: filepath, encoding: .utf8) else {
             return
         }
         
-        let document = GMarkParser().parseMarkdown(from: filecontents)
+        await renderMarkdown(filecontents)
+        self.title = fileName
+    }
+    
+    private func renderMarkdown(_ content: String) async {
+        let document = GMarkParser().parseMarkdown(from: content)
         var style = MarkdownStyle.defaultStyle()
         style.useMPTextKit = false
         style.codeBlockStyle.customRender = false
-        var vistor = GMarkupAttachVisitor(style: style)
-        let attributedText = vistor.visit(document)
-        self.markdownView.attributedText = attributedText
-        let height = attributedText.height(withWidth: style.maxContainerWidth)
-        print("Calculated height: \(height)")
+        style.maxContainerWidth = view.bounds.width - 32
+        var visitor = GMarkupAttachVisitor(style: style)
+        let attributedText = visitor.visit(document)
+        
+        await MainActor.run {
+            self.markdownView.attributedText = attributedText
+        }
     }
 
 }
